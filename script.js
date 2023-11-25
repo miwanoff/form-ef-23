89b4d3e6f3f07a9a8a8cf281953680586064044d
miwanoff <marina.iva.noffa@gmail.com>
2023-11-25 12:55:19 +0200

    Add forEach and map

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
-let labels = document.querySelectorAll("label");
+// let labels = document.querySelectorAll("label");
 
-for (let i = 0; i < labels.length; i++) {
-  let time = 0;
-  console.log(labels[i].innerHTML);
-  let letters = labels[i].innerHTML.split("");
-  console.log(letters);
-  let newInnerHTML = "";
-  for (let j = 0; j < letters.length; j++) {
-    let elm = `<span style="transition-delay: ${time}ms">${letters[j]}</span>`;
-    console.log(elm);
-    newInnerHTML += elm;
-    //
+// for (let i = 0; i < labels.length; i++) {
+//   let time = 0;
+//   console.log(labels[i].innerHTML);
+//   let letters = labels[i].innerHTML.split("");
+//   console.log(letters);
+//   let newInnerHTML = "";
+//   for (let j = 0; j < letters.length; j++) {
+//     let elm = `<span style="transition-delay: ${time}ms">${letters[j]}</span>`;
+//     console.log(elm);
+//     newInnerHTML += elm;
+//     //
+//   }
+//   console.log("newInnerHTML:" + newInnerHTML);
+//   labels[i].innerHTML = newInnerHTML;
+// }
+
+// 2
+let labels = document.querySelectorAll("label");
+labels.forEach(myFunction);
+
+function myFunction(label) {
+    label.innerHTML = label.innerHTML.split("").map(mapF).join(""); 
   }
-  console.log("newInnerHTML:" + newInnerHTML);
-  labels[i].innerHTML = newInnerHTML;
-}
+
+  function mapF(item, index) {
+    return `<span style="transition-delay: ${index}ms">${item}</span>`;
+  }
+
+  console.log(labels);
